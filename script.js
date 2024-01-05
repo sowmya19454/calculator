9d5afc6224bbf846402ec7b303b5681feaea2d7c
sowmya19454 <sowmya19454@gmail.com>
2024-01-05 23:04:37 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let display = document.getElementById('display');
+
+function appendValue(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function deleteLast() {
+    if(display.value == 'Error'){
+        display.value = "";
+    }
+  display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
